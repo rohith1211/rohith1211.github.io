612838d36c076c409b1118ce1b3b15a92f61d188
rohith1211 <84775999+rohith1211@users.noreply.github.com>
2023-11-29 01:34:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ const noBtn = document.querySelector(".no-btn");
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Yeyyy, Twaralo malli kaludham! Google lo Poyi Goodness ani Kottundri";
   gif.src =
-    "https://pa1.aminoapps.com/8702/71d8f38374cf1c93a77eecc582ff8c009eec2b9er1-500-500_hq.gif";
-  gif.src =
     "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftenor.com%2Fview%2Fanudeep-kv-nag-ashwin-naveen-polishetty-jathi-ratnalu-telugu-gif-21128115&psig=AOvVaw25UsKgkjtvcNHj_4TX0yv8&ust=1701288221572000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCLj06ta-54IDFQAAAAAdAAAAABAD";
 });
 
